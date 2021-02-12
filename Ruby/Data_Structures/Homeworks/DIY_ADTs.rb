f9227ec5885c622a13206b98d7faf9da6143c3e2
Rob Hoolan <rhoolan@gmail.com>
2021-02-12 09:43:30 +0000

    Complete DIY_ADTs Homework

--- Ruby/Data_Structures/Homeworks/DIY_ADTs.rb
+++ Ruby/Data_Structures/Homeworks/DIY_ADTs.rb
@@ -40,19 +40,38 @@ class Map
         @ivar = []
     end
 
+    def lookup(key)
+        @ivar.each do |entry|
+            return entry if entry[0] == key
+        end
+        nil
+    end
+
     def set(key, value)
+        entry = lookup(key)
+        if entry.nil?
+            @ivar << [key, value]
+        else
+            entry[1] == value
+        end
     end
 
     def get(key)
+        entry = lookup(key)
+        entry[0]
     end
 
     def get(value)
+        entry = lookup(key)
+        entry[1]
     end
 
     def delete(key)
+        entry = lookup(key)
+        @ivar.delete(entry) unless entry.nlil?
     end
 
     def show
+        @ivar
     end
-
 end
